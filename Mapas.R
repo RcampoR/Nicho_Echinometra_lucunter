fe19332441403c258196a37e9cbeb5fd172e8197
RcampoR <rcamporomero88@correo.unicordoba.edu.co>
2025-06-11 22:01:30 -0500

    mapa idoneidad, listooo

--- Mapas.R
+++ Mapas.R
@@ -14,7 +14,7 @@ ocurrencias_E_lucunter <- readr::read_delim("BD_E_lucunter_submuestreado_COL.csv
 crs(ocurrencias_E_lucunter) <- "EPSG:4326"
 
 #raster
-mapa_idoneidad <- rast("C:\\Proyecto_biologicos\\Proyectos Actuales\\Nicho_E_lucunter\\MAPAS\\capa_idoneidad_E_lucunter.tif")
+Raster_idoneidad <- rast("C:\\Proyecto_biologicos\\Proyectos Actuales\\Nicho_E_lucunter\\MAPAS\\Raster_idoneidad.tif")
 
 #MAR CARIBE
 Mar_caribe_INVEMAR <- vect("C:\\Proyecto_biologicos\\Proyectos Actuales\\Nicho_E_lucunter\\COL_shp\\mar_caribe.json") 
@@ -22,7 +22,10 @@ Mar_caribe_INVEMAR <- vect("C:\\Proyecto_biologicos\\Proyectos Actuales\\Nicho_E
 
 # MAPA CONTEXTO PUNTOS OCURRENCIA
 
-mundo <- world
+
+
+mundo <- world(path=".")
+crs(mundo) <- "EPSG:4326"
 
 
 mapa_contexto <- tm_shape(mundo, xlim = c(-100, -40), ylim = c(-60, 40)) + # Define la extensión del mapa
@@ -31,8 +34,6 @@ mapa_contexto <- tm_shape(mundo, xlim = c(-100, -40), ylim = c(-60, 40)) + # Def
   tm_shape(Colombia) + # Añade una nueva capa solo para Colombia
   tm_polygons(fill = "gray48") + # Rellena Colombia con un color específico (ej. verde oscuro)
   tm_borders(col = "black", lwd = 1.2) + # Bordes más gruesos para Colombia 
-  tm_compass(position = c("right", "top")) +
-  tm_scalebar(position = c("left", "bottom")) +
   tm_shape(Mar_caribe_INVEMAR) +
   tm_polygons(fill = "lightblue")
 
@@ -52,32 +53,92 @@ mapa_ocurrencias <- tm_shape(Mar_caribe_INVEMAR) +
                                       position = c("left", "bottom"))) +
   tm_scalebar(position = c("bottom", "right"), size = 5) +
   tm_compass(position = c("top", "right"), size = 3, type = "arrow") +
-  tm_grid() +
+  tm_graticules(lines = FALSE,
+                labels.col = "gray10") + 
   tm_add_legend(title = "LEYENDA",
                 type = "polygons",
                 labels = c("Mar Caribe colombiano", "Republica de Colombia", 
                            expression("Puntos de Ocurrencia " * italic("Echinometra lucunter"))),
                 fill = c("lightblue", "gray48", "red"),
                 fontfamily = "sans",
-                position = c(-0.4, 0.25))
+                position = c(-0.08, 0.13)) +
+  tm_layout(frame = TRUE,
+            frame.lwd = 3,
+            frame.color = "gray20")
+
+# imprimir mapa_ocurrencias
+print(mapa_ocurrencias)
+print(mapa_contexto, vp = grid::viewport(0.92, 0.3, width = 0.15, height = 0.2))
 
 
+# guardar mapa 
+dev.copy(png, 
+         filename = "mapa_distribución.png", 
+         width = 10,        # 8 pulgadas de ancho
+         height = 6,       # 6 pulgadas de alto
+         units = "in",     # Unidades en pulgadas
+         res = 1000)   
+
+dev.off()
 
 
+# MAPA CONTEXTO IDONEIDAD
 
+mapa_contexto_2 <- tm_shape(mundo, xlim = c(-100, -40), ylim = c(-60, 40)) + # Define la extensión del mapa
+  tm_fill(fill = "white") + # Rellena todos los países del mundo
+  tm_borders(col = "gray23", lwd = 0.5) + # Bordes para todos los países
+  tm_shape(Colombia) + # Añade una nueva capa solo para Colombia
+  tm_polygons(fill = "gray89") + # Rellena Colombia con un color específico (ej. verde oscuro)
+  tm_borders(col = "black", lwd = 1.2) + # Bordes más gruesos para Colombia 
+  tm_shape(Mar_caribe_INVEMAR) +
+  tm_polygons(fill = "lightblue")
 
+# Mapa de idoneidad de habitat 
 
-
-
+Mapa_idoneidad <-  tm_shape(Mar_caribe_INVEMAR) +
+  tm_polygons(fill = "lightblue") +
 tm_shape(Colombia) +
-  tm_fill() + 
-  tm_shape(mapa_idoneidad) +
-  tm_raster()
+  tm_fill(fill = "gray89") + 
+  tm_shape(Raster_idoneidad) +
+  tm_raster(col.scale = tm_scale(values = "brewer.yl_or_rd"),
+            col.legend = tm_legend(title = expression("Probabilidad de presencia"),
+                                   position = c(-0.098, 0.348))) +
+ tm_scalebar(position = c("bottom", "right"), text.size = 0.5) +
+   tm_compass(position = c("top", "right"), size = 3, type = "arrow") +
+   tm_graticules(lines = FALSE,
+                 labels.col = "gray10") +
+   tm_add_legend(title = "LEYENDA",
+                 type = "polygons",
+                 labels = c("Mar Caribe colombiano", "Republica de Colombia"),
+                 fill = c("lightblue", "gray89"),
+                 fontfamily = "sans",
+                 position = c(-0.098, 0.348)) +
+   tm_layout(frame = TRUE,
+             frame.lwd = 3,
+             frame.color = "gray20")
+ 
+
+# imprimir mapa_idoneidad
+print(Mapa_idoneidad)
+print(mapa_contexto_2, vp = grid::viewport(0.92, 0.3, width = 0.15, height = 0.2))
+
+# guardar mapa 
+dev.copy(png, 
+         filename = "mapa_idoneidad.png", 
+         width = 10,        # 8 pulgadas de ancho
+         height = 6,       # 6 pulgadas de alto
+         units = "in",     # Unidades en pulgadas
+         res = 1000)   
+
+dev.off()
 
 
+ # visualizar
+tmap_mode("view")
 
+# estatico
 
-
+tmap_mode("plot")
 
 
 
