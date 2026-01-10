26050d281ace495e0f137550d6cc71903ba521f5
RcampoR <rcamporomero88@correo.unicordoba.edu.co>
2026-01-10 07:30:12 -0500

    Escenario optimistas ssp1

--- 7_escenario_optimista.R
+++ 7_escenario_optimista.R
@@ -1,10 +1,10 @@
-library(terra) # raster y vectores
-library(tmap) # mapas tematicos
-library(tidyverse) # maniulacion de datos y graficas
-library(geodata) # datos espaciales en linea
-library(here)# control de direcciones
+library(terra) 
+library(tmap) 
+library(tidyverse)
+library(geodata) 
+library(here)
 library(randomForest)
-library(mgcv) # GAM
+library(mgcv)
 library(predicts)
 
 
